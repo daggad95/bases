50bcf4e4282b62c9dd54613f3f96db3b94153bd4
david <daggad95@gmail.com>
2017-12-31 13:58:09 -0800

    lol

---
+++ Main.java
@@ -0,0 +1,16 @@
+import processing.core.PApplet;
+
+public class Test extends PApplet {
+	public static void main(String args[]) {
+		PApplet.main("Test");
+	}
+
+	public void setup() {
+	}
+
+	public void run() {}
+
+	public void settings() {
+		size(300, 300);	
+	}
+}
